0dbe9a276ef9f0cb15638adecfe7f746d7433fc5
Greit <greitopingari@gmail.com>
2021-11-17 16:05:55 +0100

    Update

--- home/models.py
+++ home/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from wagtail.core.fields import StreamField
-
-from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, RichTextField
 from wagtail.core import blocks
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel,RichTextField
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 
-class HomePage(Page):
 
+class HomePage(Page):
     template = "home/home_page.html"
 
     banner_text = models.CharField(max_length=200, null=True, blank=True)
@@ -17,8 +15,8 @@ class HomePage(Page):
     banner_text_after_colored = models.CharField(max_length=200, null=True, blank=True)
 
     homePage_first_Title = models.CharField(max_length=200, null=True, blank=True)
-    homePage_first_subTitle = models.CharField(max_length=200,null=True,blank=True)
-    homePage_first_secondSubTitle = models.CharField(max_length=200,null=True,blank=True)
+    homePage_first_subTitle = models.CharField(max_length=200, null=True, blank=True)
+    homePage_first_secondSubTitle = models.CharField(max_length=200, null=True, blank=True)
     homePage_first_description = RichTextField(null=True, blank=False)
     homePage_first_Image = models.ForeignKey(
         "wagtailimages.Image",
@@ -31,14 +29,14 @@ class HomePage(Page):
     homePage_services_title = models.CharField(max_length=200, null=True, blank=True)
     homePage_services_subTitle = models.CharField(max_length=200, null=True, blank=True)
     homePage_services_content = StreamField([
-    ('Card', blocks.StructBlock([
-        ('card_image', ImageChooserBlock(required=False)),
-        ('card_title',blocks.CharBlock()),
-    ]))
-    ],null=True,blank=False)
+        ('Card', blocks.StructBlock([
+            ('card_image', ImageChooserBlock(required=False)),
+            ('card_title', blocks.CharBlock()),
+        ]))
+    ], null=True, blank=False)
 
     homePage_third_Title = models.CharField(max_length=200, null=True, blank=True)
-    homePage_third_subTitle = models.CharField(max_length=200,null=True,blank=True)
+    homePage_third_subTitle = models.CharField(max_length=200, null=True, blank=True)
     homePage_third_description = RichTextField(null=True, blank=False)
     homePage_third_Image = models.ForeignKey(
         "wagtailimages.Image",
@@ -48,17 +46,16 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
     )
 
-
     reels_content = StreamField([
-    ('Reels', blocks.StructBlock([
-        ('reel_image', ImageChooserBlock(required=False)),
-        ('reel_link', blocks.CharBlock()),
-        ('reel_title',blocks.CharBlock()),
-    ]))
-    ],null=True,blank=False)
+        ('Reels', blocks.StructBlock([
+            ('reel_image', ImageChooserBlock(required=False)),
+            ('reel_link', blocks.CharBlock()),
+            ('reel_title', blocks.CharBlock()),
+        ]))
+    ], null=True, blank=False)
 
     homePage_fourth_Title = models.CharField(max_length=200, null=True, blank=True)
-    homePage_fourth_subTitle = models.CharField(max_length=200,null=True,blank=True)
+    homePage_fourth_subTitle = models.CharField(max_length=200, null=True, blank=True)
     homePage_fourth_description = RichTextField(null=True, blank=False)
     homePage_fourth_Image = models.ForeignKey(
         "wagtailimages.Image",
@@ -67,19 +64,16 @@ class HomePage(Page):
         related_name="+",
         on_delete=models.SET_NULL,
     )
- 
-
-   
 
     homePage_partners_title = models.CharField(max_length=200, null=True, blank=True)
     homePage_partners_subtitle = models.CharField(max_length=200, null=True, blank=True)
     homePage_partners_description = RichTextField(null=True, blank=False)
 
     partners_content = StreamField([
-    ('Partners', blocks.StructBlock([
-        ('partners_logo', ImageChooserBlock(required=False)),
-    ]))
-    ],null=True,blank=False)
+        ('Partners', blocks.StructBlock([
+            ('partners_logo', ImageChooserBlock(required=False)),
+        ]))
+    ], null=True, blank=False)
 
     content_panels = Page.content_panels + [
         FieldPanel("banner_text"),
